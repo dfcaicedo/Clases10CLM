ab6cd7409b645c21e39522d19d2ea07ac958216b
Daniel Felipe Caicedo T <dfcaicedo@gmail.com>
2021-10-05 18:01:08 -0500

    Add files via upload

--- calculadora.js
+++ calculadora.js
@@ -9,7 +9,7 @@ function decimalabinario(decimales){
     }
     while (resp.length < 8)
         resp = "0" + resp;
-    console.log("Decimal a Binario" + resp);
+ //   console.log("Decimal a Binario" + resp);
     return resp;
 }
 function multi (a,b){
@@ -17,11 +17,13 @@ function multi (a,b){
     while (a.length>0){
         var ultimoa;
         var ultimob;
-        ultimoa=Number(a.substring(a.length-1,1));
-        ultimob=Number(b.substring(b.length-1,1));
+        ultimoa=Number(a.substring(a.length-1,a.length));
+        ultimob=Number(b.substring(b.length-1,b.length));
+        console.log(a.length.toString() + ": ultimoa:" + ultimoa + "  ultimob:" + ultimob + " resultado:" + (ultimoa * ultimob).toString());
         resultado = (ultimoa * ultimob).toString() + resultado;
-        a = a.Substring(0, a.Length - 1);
-        b = b.Substring(0, b.Length - 1);
+        
+        a=a.substring(0,(a.length-1));
+        b=b.substring(0,(b.length-1)); 
     }
     return resultado;
 }
@@ -31,15 +33,15 @@ function invertirnumero (numero){
     return numero.replace("2","0");
 }
 function binarioadecimal(valor){
-    console.log(valor);
+   // console.log(valor);
     var resp=0;
     var a=0;
     for (a=0;a<valor.length;a++){
         var nume=Number(valor.substring(valor.length-1-a,valor.length-a));
-        console.log(a.toString() + ":" + nume);
+       // console.log(a.toString() + ":" + nume);
         resp=resp+nume*Math.pow(2,a);
     }
-    console.log("Binario a decimal " + resp);
+   // console.log("Binario a decimal " + resp);
     return resp.toString();
 
 }
@@ -50,23 +52,48 @@ function sumaOR(var1, var2){
 }
 //Math.floor Encuentra el entero menor más cercano al resultado
 function accionboton(){
-    console.log("Capturó el click del botón");
+   // console.log("Capturó el click del botón");
     var text1=document.getElementById("text1");// resumen del document.getelementbyid, para no escribirlo siempre
-    //vamos a pasar a binario lo del text1 al itext1
     document.getElementById("itext1").value=decimalabinario(text1.value);
-    var mtext1=document.getElementById("mtext1");
-    var nmask=document.getElementById("nmask");
-    var a=0;
+    var text2=document.getElementById("text2");// resumen del document.getelementbyid, para no escribirlo siempre
+    document.getElementById("itext2").value=decimalabinario(text2.value);
+    var text3=document.getElementById("text3");// resumen del document.getelementbyid, para no escribirlo siempre
+    document.getElementById("itext3").value=decimalabinario(text3.value);
+    var text4=document.getElementById("text4");// resumen del document.getelementbyid, para no escribirlo siempre
+    document.getElementById("itext4").value=decimalabinario(text4.value);
     var mascara="";
     for (a=0;a<nmask.value;a++)
         mascara=mascara+"1";
     while (mascara.length<32)
         mascara=mascara + "0";
-    document.getElementById("mbtext1").value=mascara.substring(0,8);
-    document.getElementById("mtext1").value=binarioadecimal(document.getElementById("mbtext1").value)
-    
+    console.log(mascara);
+    var mbtext1=document.getElementById("mbtext1");
+    var mbtext2=document.getElementById("mbtext2");
+    var mbtext3=document.getElementById("mbtext3");
+    var mbtext4=document.getElementById("mbtext4");
+   mbtext1.value=mascara.substring(0,8);
+   mbtext2.value=mascara.substring(8,16);
+   mbtext3.value=mascara.substring(16,24);
+   mbtext4.value=mascara.substring(24,32);
+   var mtext1=document.getElementById("mtext1");
+   var mtext2=document.getElementById("mtext2");
+   var mtext3=document.getElementById("mtext3");
+   var mtext4=document.getElementById("mtext4");
+   mtext1.value=binarioadecimal(mbtext1.value);
+   mtext2.value=binarioadecimal(mbtext2.value);
+   mtext3.value=binarioadecimal(mbtext3.value);
+   mtext4.value=binarioadecimal(mbtext4.value);
+    //Hallar el segmento
+    var stext1=document.getElementById("stext1");
+    stext1.value=multi(mbtext1.value,document.getElementById("itext1").value)
+    var stext2=document.getElementById("stext2");
+    stext2.value=multi(mbtext2.value,document.getElementById("itext2").value)
+    var stext3=document.getElementById("stext3");
+    stext3.value=multi(mbtext3.value,document.getElementById("itext3").value)
+    var stext4=document.getElementById("stext4");
+    stext4.value=multi(mbtext4.value,document.getElementById("itext4").value)
 
 }
 
-var boton1=document.getElementById("btn"); // Seleccionar el Boton
-boton1.addEventListener("click",accionboton); // Aqui agrego el que va a hacer cuando le den click
+var botons=document.getElementById("btn"); // Seleccionar el Boton
+botons.addEventListener("click",accionboton); // Aqui agrego el que va a hacer cuando le den click
